706fadd5eb0a41afdebfded97814b35e1e8f2c2b
thelastmedici <christianopeyemijoshua@gmail.com>
2024-08-20 23:26:05 +0100

    added solution to task

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 """Write a coroutine called async_generator that takes no arguments.
 
 The coroutine will loop 10 times, each time asynchronously wait 1 second,
